b11af3229cf0af2a8d1e3199aed431557dbc4c5f
Bavithran M <79276699+bavicnative@users.noreply.github.com>
2024-12-18 22:02:43 +0530

    Update terraform.sh

--- Infrastructure/AWS/terraform.sh
+++ Infrastructure/AWS/terraform.sh
@@ -6,7 +6,7 @@ sudo apt update
 sudo apt upgrade -y
 mkdir -p ~/terraform
 cd ~/terraform
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+wget https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform -y
 
